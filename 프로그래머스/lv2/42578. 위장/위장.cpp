a509136607739cd31f7ea2aa510f78806ba1026c
HMCLMINU <97032862+HMCLMINU@users.noreply.github.com>
2023-02-23 18:08:32 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string ,int> m;
+    
+    for (int i=0; i<clothes.size(); i++){
+        m[clothes[i][1]]++;
+    }
+    for (auto& iter:m){
+        answer*=(iter.second+1);
+    }
+    
+    
+    return --answer;
+}
