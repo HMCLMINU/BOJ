36b93f773f70b6a975f82db288a149062afcf6d6
HMCLMINU <97032862+HMCLMINU@users.noreply.github.com>
2023-02-28 15:37:40 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.17 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int maxKeySize;
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    vector<int> answer;
+    unordered_map<char, int> hash;
+    
+    for (auto& i:keymap) {
+        for (int j=0; j<i.size(); j++){
+            if (hash[i[j]]!=0 && hash[i[j]]>j+1)
+                hash[i[j]]=j+1;
+            else if (!hash[i[j]]) hash[i[j]]=j+1;
+        }
+    }
+    
+    for (auto& i:targets){
+        int sum=0;
+        bool ch=false;
+        for (int j=0; j<i.size(); j++){
+            if (!hash[i[j]]) {
+                ch=true;
+                break;
+            }
+            else sum+=hash[i[j]];
+        }
+        if (ch) answer.push_back(-1);
+        else answer.push_back(sum);
+    }
+    
+    return answer;
+}
