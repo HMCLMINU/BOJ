bc05307cac3589e4edbd8ff7c95fff38a71cf640
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2023-03-10 00:42:34 +0900

    [unrated] Title: 옹알이 (2), Time: 0.03 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -1,39 +1,26 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <unordered_map>
 
 using namespace std;
 
-vector<string> v({"aya", "ye", "woo", "ma"});
-unordered_map<string, int> hash;
-
-void registString(string str, string prev) 
-{
-    if (str.size() > 0 && str.size() <= 30) {
-        ::hash[str] = 1;
-       
-        if (prev != "aya")
-            registString(str+"aya", "aya");
-        if (prev != "ye")
-            registString(str+"ye", "ye");
-        if (prev != "woo")
-            registString(str+"woo", "woo");
-        if (prev != "ma")
-            registString(str+"ma", "ma");
-    }
-}
-
 int solution(vector<string> babbling) {
     int answer = 0;
-    
-    registString("aya", "aya");
-    registString("ye", "ye");
-    registString("woo", "woo");
-    registString("ma", "ma");
-    
-    for (auto i : babbling) {
-        if (::hash[i]) answer++;
+
+    for(int i;i<babbling.size();i++)
+    {
+        string temp1="";
+        string temp2="";
+        for(char c:babbling[i])
+        {
+            temp1+=c;
+            if(temp1 == "aya"||temp1 == "ye"||temp1 == "woo"||temp1 == "ma")
+            {
+                if(temp2 == temp1) break;
+                temp2=temp1;
+                temp1="";
+            }
+        }
+        if(temp1.size()==0) answer++;
     }
     return answer;
 }
