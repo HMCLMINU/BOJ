5ca7af8db949549b96ace8d2d4ef98e68aa13105
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2023-03-12 23:08:45 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 0.58 ms, Memory: 3.92 MB -BaekjoonHub

--- 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.cpp
+++ 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.cpp
@@ -13,16 +13,15 @@ int solution(int n, vector<int> cores) {
     if (n <= num_cores) return n;
     // 시간이 주어질 때 몫 만큼 해결
     // 2개 코어, 10000시간, 50000개 작업
-    // 25000 * 10000 시간
+    // n / cores.size() * max time
     ll left = 1;
     ll right = *max_element(cores.begin(), cores.end()) * n / cores.size();
     ll mid = 0;
-    // sort(cores.begin(), cores.end());
+
     while (left <= right) {
         mid = (left + right) / 2;
         int workCnt = num_cores;
         int currWorkCnt = 0;
-        int last = 0;
         
         for (int i=0; i<num_cores; i++) {
             workCnt += (mid / cores[i]);
@@ -44,10 +43,13 @@ int solution(int n, vector<int> cores) {
                 if (mid % cores[i] == 0) {
                     workCnt++;
                 }
-                if (workCnt == n)
+                if (workCnt == n) {
                     return i+1;
+                }
             }
+            right = mid - 1;
         }
+        
     }
 
     return answer;
