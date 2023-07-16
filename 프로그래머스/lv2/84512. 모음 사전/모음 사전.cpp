bf06b364260ae7d6292b307137262b11365d4d77
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2023-07-16 19:19:57 +0900

    [level 2] Title: 모음 사전, Time: 0.09 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
@@ -1,53 +1,33 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <set>
-#include <iostream>
 
 using namespace std;
 
 vector<char> v{'A', 'E', 'I', 'O', 'U'};
+int cnt = -1;
+string target="";
+int answer = 0;
 
-int solution(string word) {
-    int answer = 0;
-    int n = v.size();
-    int count = 0;
-    unordered_map<string, int> um;
-    string ss;
+void dfs(string w)
+{
+    cnt++;
     
-    for (int i=0; i<n; i++) {
-        ss += v[i];
-        count++;
-        um[ss] = count;
-        
-        for (int ii=0; ii<n; ii++) {
-            ss += v[ii];
-            count++;
-            um[ss] = count;
-            
-            for (int iii=0; iii<n; iii++) {
-                ss += v[iii];
-                count++;
-                um[ss] = count;
-                for (int iiii=0; iiii<n; iiii++) {
-                    ss += v[iiii];
-                    count++;
-                    um[ss] = count;
-                    for (int iiiii=0; iiiii<n; iiiii++) {
-                        ss += v[iiiii];
-                        count++;
-                        um[ss] = count;
-                        ss.pop_back();
-                    }
-                    ss.pop_back();
-                }
-                ss.pop_back();
-            }
-            ss.pop_back();
-        }
-        ss.pop_back();   
+    if (w == target) {
+        answer = cnt;
+        return;
     }
     
-    answer = um[word];
+    if (w.size() >= 5) return;
+    
+    for (int i=0; i<5; i++) {
+        dfs(w + v[i]);
+    }    
+}
+
+int solution(string word) {
+    target = word;     
+    dfs("");
+
     return answer;
 }
