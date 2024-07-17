4fbe0059ef6badb6be03be0b99f38336e1125aa9
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-07-17 23:17:40 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -2,5 +2,7 @@ def solution(n):
     answer = 0
     for num in str(n):
         answer += int(num)
-
+    
+    # return sum(map(int, str(n)))
+    return sum(int(i) for i in str(n))
     return answer
