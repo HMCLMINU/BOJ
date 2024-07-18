25e2448ede57c480406262c5cab961545016574b
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-07-18 23:36:41 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 435.35 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -2,7 +2,13 @@ from math import *
 
 def solution(n):
     
-    if int(sqrt(n)) == sqrt(n):
-        return (sqrt(n) + 1)**2
-    else:
-        return -1
+    # if int(sqrt(n)) == sqrt(n):
+    #     return (sqrt(n) + 1)**2
+    # else:
+    #     return -1
+    
+    for i in range(1, int(sqrt(n))+1):
+        if i*i == n:
+            return (i+1)**2
+    
+    return -1
