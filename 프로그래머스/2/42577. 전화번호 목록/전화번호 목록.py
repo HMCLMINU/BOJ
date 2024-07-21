c32227ebaa4e76b20278616df30db698bb6f12f1
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-07-21 13:00:13 +0900

    [level 2] Title: 전화번호 목록, Time: 89.85 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,19 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    for idx, value in enumerate(phone_book):
-        if idx == len(phone_book)-1:
-            break
-        is_find = phone_book[idx+1].find(value)
-        if is_find != -1 and is_find == 0:
-            return False
+    # Sol. 1
+#     for idx, value in enumerate(phone_book):
+#         if idx == len(phone_book)-1:
+#             continue
+#         is_find = phone_book[idx+1].find(value)
+#         if is_find != -1 and is_find == 0:
+#             return False
         
+#     return True
+
+    # Sol. 2 
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    
     return True
