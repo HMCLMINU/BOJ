d06c0bdb7f6873ca51e851bb24c9a1f96415fa4e
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-07-21 12:09:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.09 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,21 @@
+import collections
+
 def solution(participant, completion):
     answer = ''
-    dict = {}
-    for name in participant:
-        dict[name] = 0
-
-    for name in participant:
-        dict[name]+=1
+#     dict = {}
+#     for name in participant:
+#         if not dict.get(name, 0):
+#             dict[name] = 1
+#         else:
+#             dict[name]+=1
     
-    for name in completion:
-        dict[name]-=1
+#     for name in completion:
+#         dict[name]-=1
     
-    for key, value in dict.items():
-        if value:
-            return key
-        
+#     for key, value in dict.items():
+#         if value:
+#             return key
+    
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+    
