d4886e502bce081435fcc9c322e009fce3ff4799
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-05 20:00:08 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,19 @@
-from math import ceil
+from math import *
 
 def solution(progresses, speeds):
     daysLeft = list(map(lambda x: (ceil((100 - progresses[x]) / speeds[x])), range(len(progresses))))
     count = 1
-    retList = []
-
+    answer = []
     for i in range(len(daysLeft)):
-        try:
-            if daysLeft[i] < daysLeft[i + 1]:
-                retList.append(count)
+        try: 
+            if daysLeft[i] < daysLeft[i+1]:
+                answer.append(count)
                 count = 1
             else:
-                daysLeft[i + 1] = daysLeft[i]
+                daysLeft[i+1] = daysLeft[i]
                 count += 1
+                
         except IndexError:
-            retList.append(count)
-
-    return retList
+            answer.append(count)
+            
+    return answer 
