6175676ba262eb1df907121165a2421660094a82
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-06 21:59:10 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,25 @@
 from math import *
+from collections import deque
 
 def solution(progresses, speeds):
     daysLeft = list(map(lambda x: (ceil((100 - progresses[x]) / speeds[x])), range(len(progresses))))
     count = 1
     answer = []
-    for i in range(len(daysLeft)):
-        try: 
-            if daysLeft[i] < daysLeft[i+1]:
-                answer.append(count)
-                count = 1
+    q = deque()
+    q.append(daysLeft[0])
+    idx = 1
+    while len(q) != 0:
+        v = q[0]
+        try:
+            if v >= daysLeft[idx]:
+                q.append(daysLeft[idx])
             else:
-                daysLeft[i+1] = daysLeft[i]
-                count += 1
-                
+                answer.append(len(q))
+                q.clear()
+                q.append(daysLeft[idx])
         except Exception:
-            answer.append(count)
-            
+            answer.append(len(q))
+            break
+        idx += 1
+
     return answer 
