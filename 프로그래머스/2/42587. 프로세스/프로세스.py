e5d52742d26ca739cd52622f1036470fd8904113
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-08 23:35:13 +0900

    [level 2] Title: 프로세스, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    
+    q = deque()
+    for idx,v in enumerate(priorities):
+        q.append([idx, v])
+        
+    priorities.sort(reverse=True)
+    idx = 0
+    while len(q) > 0:
+        v = q.popleft()
+        # 현재 작업의 우선순위가, 가장 먼저 해야할 작업인지 확인
+        if v[1] == priorities[idx]: 
+            # location과 같은지 확인
+            if v[0] == location:
+                return idx+1
+
+            idx += 1
+        # 아니라면 다시 넣어야 함    
+        else:
+            q.append(v)
+    
