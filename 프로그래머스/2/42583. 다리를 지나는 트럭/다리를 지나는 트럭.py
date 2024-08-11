763b089423d024ad38ebfde82fe219db91b9fd06
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-11 16:47:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 334.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,26 +1,14 @@
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    in_time = [] # 트럭 진입 시간
-    curr_w = [] # 다리 위 트럭
-    time = 1
-    
-    while True:
-        if in_time:
-            if time - in_time[0] >= bridge_length:
-                in_time.pop(0)
-                curr_w.pop(0) 
-            
-        # curr_w가 weight 이하인 경우 현재 시간 큐에 넣기
+    q=[0]*bridge_length
+    sec=0
+    curr_w = 0
+    while q:
+        sec+=1
+        curr_w -= q.pop(0)
         if truck_weights:
-            if sum(curr_w)+truck_weights[0] <= weight:
-                curr_w.append(truck_weights[0]) 
-                truck_weights.pop(0)
-                in_time.append(time)
-        
-        if len(curr_w) == 0:
-            answer = time
-            break
-            
-        time += 1
-        
-    return answer
+            if curr_w+truck_weights[0]<=weight:
+                curr_w += truck_weights[0]
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+    return sec
