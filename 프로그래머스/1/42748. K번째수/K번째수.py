77b286917453a00a641600e85bf8c0be9f116202
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-13 00:02:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,8 @@
 def solution(array, commands):
     answer = []
-    for idx, command in enumerate(commands):
-            answer.append(sorted(array[command[0]-1:command[1]])[command[2]-1])
-        
-    return answer
+    #for idx, command in enumerate(commands):
+            #answer.append(sorted(array[command[0]-1:command[1]])[command[2]-1])
+    
+    return list(map(lambda x : sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+    #return answer
+    
