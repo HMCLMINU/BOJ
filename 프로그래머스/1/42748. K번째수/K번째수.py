f347d7bb7b1c904a8baee19addcdf4708fd7eda3
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-08-13 00:00:35 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,6 @@
 def solution(array, commands):
     answer = []
     for idx, command in enumerate(commands):
-        try:
             answer.append(sorted(array[command[0]-1:command[1]])[command[2]-1])
-        except Exception:
-            print(idx, command[0]-1, command[1]-1, command[2]-1)
         
     return answer
