58ada5d4f59aa837525039d9e2ebdcd29915f9dc
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-09-20 15:21:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,8 +1,11 @@
 def solution(s):
-    answer = ''
-    n = s.split(' ')
-    n.sort(key=int)
-    answer+=n[0]
-    answer+=' '
-    answer+=n[-1]
-    return answer
+    # answer = ''
+    # n = s.split(' ')
+    # n.sort(key=int)
+    # answer+=n[0]
+    # answer+=' '
+    # answer+=n[-1]
+    
+    n = list(map(int, s.split(' ')))
+    return str(min(n)) + ' ' + str(max(n))
+    # return answer
