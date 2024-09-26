1622eafd6cac54d00757c20d57a34424c54ad630
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-09-26 23:50:49 +0900

    [level 2] Title: 배달, Time: 4.53 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -0,0 +1,38 @@
+import heapq
+
+INF = 1e9
+
+def solution(N, road, K):
+    answer = 0 
+    
+    times = [int(INF) for _ in range(N+1)]
+
+    # set graph 
+    graph = [[] for _ in range(N+1)]
+    for info in road:
+        graph[info[0]].append((info[1], info[2])) # (노드, 시간)
+        graph[info[1]].append((info[0], info[2]))
+    
+    # dijkstra
+    q = []
+    heapq.heappush(q, (1, 0)) # (노드, 시간) 1번 노드 시작
+    times[1] = 0
+    
+    while q:
+        now, time = heapq.heappop(q)
+        
+        if times[now] < time:
+            continue
+        
+        for adj in graph[now]:
+            cost = time + adj[1]
+            # 거쳐 온 경우가 현재보다 작다면 시간 업데이트 
+            if cost < times[adj[0]]:
+                times[adj[0]] = cost
+                heapq.heappush(q, (adj[0], cost))
+    
+    for time in times:
+        if time <= K:
+            answer+=1
+    
+    return answer
