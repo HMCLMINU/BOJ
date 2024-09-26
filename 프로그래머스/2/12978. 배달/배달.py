d7d040b0e32c1dd108ac630ec739f3828deda876
MIN U <97032862+HMCLMINU@users.noreply.github.com>
2024-09-26 23:58:45 +0900

    [level 2] Title: 배달, Time: 2.23 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -15,11 +15,11 @@ def solution(N, road, K):
     
     # dijkstra
     q = []
-    heapq.heappush(q, (1, 0)) # (노드, 시간) 1번 노드 시작
+    heapq.heappush(q, (0, 1)) # (시간, 노드) 1번 노드 시작
     times[1] = 0
     
     while q:
-        now, time = heapq.heappop(q)
+        time, now = heapq.heappop(q)
         
         if times[now] < time:
             continue
@@ -29,7 +29,7 @@ def solution(N, road, K):
             # 거쳐 온 경우가 현재보다 작다면 시간 업데이트 
             if cost < times[adj[0]]:
                 times[adj[0]] = cost
-                heapq.heappush(q, (adj[0], cost))
+                heapq.heappush(q, (cost, adj[0]))
     
     for time in times:
         if time <= K:
